582609c90d9e5a76f3f68e444c5abf94476df316
Felipe Blanco Muzzolon <felipeblanco114@gmail.com>
2021-09-27 11:06:42 -0300

    list-item-container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
 
 const App = () => {
     return (
         <div className='app'>
             <NavBar />
+            <ItemListContainer />
         </div>
     )
 }
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ItemListContainer.css';
+
+const ItemListContainer = () => {
+    return (
+        <div>
+            <h3>Item List Container</h3>
+        </div>
+    )
+}
+
+export default ItemListContainer;
