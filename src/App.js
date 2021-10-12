e10f73284f0740b9c37cd237b78069e271074b40
Felipe Blanco Muzzolon <felipeblanco114@gmail.com>
2021-10-12 14:23:46 -0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
-import { data } from './constants/products';
 
 // const { products } = data;
 
 const App = () => {
 
-    const [ itemList, setItemList ] = useState([]);
-    const [ loading, setLoading ] = useState(true);
-
-    const getItems = async () => {
-        try {
-            const { products } = await data;
-            setItemList(products);
-            setLoading(false);
-        }
-        catch (err) {
-            console.log(err);
-        }
-    }
-
-    useEffect(() => {
-        setTimeout(() => {
-            getItems();
-        }, [1000])
-    }, [itemList]);
-
     return (
         <div className='app'>
             <NavBar />
-            <ItemListContainer products={itemList} loading={loading} />
+            <ItemListContainer />
         </div>
     )
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import ItemCount from '../Buttons/ItemCount/ItemCount';
+import React, { useState, useEffect } from 'react';
+import { data } from '../../constants/products';
 import ItemList from '../ItemList/ItemList';
 import './ItemListContainer.css';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const ItemListContainer = ({ products, loading }) => {
+const ItemListContainer = () => {
+
+    const [ itemList, setItemList ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+
+    const getItems = async () => {
+        try {
+            const { products } = await data;
+            setItemList(products);
+            setLoading(false);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            getItems();
+        }, [1000])
+    }, [itemList]);
 
     return (
         <div className={ loading ? 'loading' : 'item-list-container'}>
-            { loading ? <CircularProgress style={{ 'color': 'rgb(155, 0, 36)' }} /> : <ItemList products={products} />}
-            {/* <ItemCount initial={1} stock={5} /> */}
+            { loading ? <CircularProgress style={{ 'color': 'rgb(155, 0, 36)' }} /> : <ItemList products={itemList} />}
         </div>
     )
 }
