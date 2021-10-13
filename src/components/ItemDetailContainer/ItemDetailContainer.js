83b8774bdf77f8ec0d74b94cecdff58bced368e1
Felipe Blanco Muzzolon <felipeblanco114@gmail.com>
2021-10-13 12:36:31 -0300

    fix

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -24,7 +24,7 @@ const ItemDetailContainer = ({ id }) => {
         setTimeout(() => {
             getItems();
         }, [1000])
-    }, [item]);
+    });
 
     return (
         <div className={loading ? 'loading' : 'item-detail-container'}>
