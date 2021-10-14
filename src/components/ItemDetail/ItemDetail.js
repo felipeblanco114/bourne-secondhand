3ada5a3d038a9d5d57be282bc9205e40444729d5
Felipe Blanco Muzzolon <felipeblanco114@gmail.com>
2021-10-14 15:52:39 -0300

    details-style

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -23,8 +23,20 @@ const ItemDetail = ({ id, item }) => {
                                 <p>{item.description}</p>
                                 <h2>${item.price}</h2>
                             </div>
-                            <div>
-                                <ItemCount initial={1} stock={item.stock} onAdd={onAdd} category={item.category} />
+                            <div className='details-bottom'>
+                                <div className='talles-estado'>
+                                    <div>
+                                        <h3>{item.talle}</h3>
+                                    </div>
+                                    <div className='medidas'>
+                                        <p>LARGO: {item.alto}</p>
+                                        <p>ANCHO: {item.ancho}</p>
+                                        <p>ESTADO: {item.estado}</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <ItemCount initial={1} stock={item.stock} onAdd={onAdd} category={item.category} />
+                                </div>
                             </div>
                         </div>
                     </div>
