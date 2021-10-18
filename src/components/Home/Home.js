736399a7128b05af6f78d08f59eb84129b44e8b2
Felipe Blanco Muzzolon <felipeblanco114@gmail.com>
2021-10-18 16:21:37 -0300

    home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,59 @@
+import React from 'react';
+import './Home.css';
+import img1 from '../../assets/img/bourne-girls-style2.jpg';
+import img2 from '../../assets/img/bourne-clothes-city.jpg';
+import img3 from '../../assets/img/bourne-clothes2.jpg';
+import {
+    BrowserRouter as Router,
+    useHistory,
+    Switch,
+    Route
+  } from "react-router-dom";
+
 const Home = () => {
+
+    const history = useHistory();
+
+    const handleHome = (link) => {
+        history.push(link);
+    }
+
     return (
-        <div>
-            Home
+        <div className='home-container'>
+            <div className='logo'>
+                <h1>Bourne</h1>
+                <h2>Second-Hand</h2>
+            </div>
+            <div className='home-img-container'>
+                <div onClick={() => handleHome('products/')} className='box'>
+                    <div className='img-box'>
+                        <img src={img1} alt='bourne' />
+                    </div>
+                    <div className='content'>
+                        <div>
+                            <h2>PRODUCTOS</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className='box'>
+                    <div className='img-box'>
+                        <img src={img2} alt='bourne' />
+                    </div>
+                    <div className='content'>
+                        <div></div>
+                    </div>
+                </div>
+                <div onClick={() => handleHome('contact/')} className='box'>
+                    <div className='img-box'>
+                        <img src={img3} alt='bourne' />
+                    </div>
+                    <div className='content'>
+                        <div>
+                            <h2>CONTACTO</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
