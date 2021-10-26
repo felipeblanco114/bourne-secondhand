7a9fa1ed0f2471d383d7614da286190ccb65d1ca
Felipe Blanco Muzzolon <felipeblanco114@gmail.com>
2021-10-26 16:50:29 -0300

    remove

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,23 +2,36 @@ import React, { useContext } from 'react';
 import {CartContext} from '../../contexts/CartContext';
 import './Cart.css'
 import {useHistory} from 'react-router-dom';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const Cart = () => {
 
     const history = useHistory();
 
-    const { cart } = useContext(CartContext);
+    const { cart, setCart, setCartId, cartId } = useContext(CartContext);
 
     const handleLink = (link) => {
         history.push(link)
     }
+
+    const removeToCart = (product, id) => {
+        const exist = cart.find((x) => x.id === product.id);
+        const existId = cartId.find((xid) => xid.id === id);
+        if(exist || existId) {
+            setCart(cart.filter((x) => x.id !== product.id));
+            setCartId(cartId.filter((xid) => xid.id !== id));
+        }
+    }
     return (
         <div className='cart-container'>
         { cart.length ? cart.map((cartItem) => (
-            <div className='cart-item-container' key={cartItem.id} onClick={() => handleLink(`/products/${cartItem.id}`)}>
-                <h2>{cartItem.title}</h2>
+            <div className='cart-item-container' key={cartItem.id} >
+                <div>
+                    <h3 onClick={() => handleLink(`/products/${cartItem.id}`)}>{cartItem.title}</h3>
+                </div>
+                <DeleteIcon onClick={() => removeToCart(cartItem)}>ELIMINAR</DeleteIcon>
             </div>
-        )) : 'No se encuentran elementos en el cart'}
+        )) : 'No se encuentran elementos en el carro.'}
         </div>
     )
 }
